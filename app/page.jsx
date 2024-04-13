3f5ad3de3b835f344860b55bdb8361d825fa9092
Noel Guillen <noelblas274@gmail.com>
2024-04-13 03:01:39 -0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+import Preview from "./home/home/page";
+export const metadata = {
+  title:
+    "Preview || Bostami - Tailwind CSS  Personal Portfolio React Nextjs Template",
+};
+export default function Home() {
+  return <Preview />;
+}
